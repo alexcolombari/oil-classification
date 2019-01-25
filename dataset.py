eb629f60291ec5587dc1bd318f141136e895fcc8
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-01-25 11:05:30 -0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,18 +1,25 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from keras.utils.np_utils import to_categorical
 from sklearn.utils.class_weight import compute_class_weight
 
 def load_dataset():
     #data_folder = "/opt/data_repository/oil_samples/"
     #file_to_open = data_folder + "half-samples.pkl"
 
-    
     file_to_open = "laminas-dez.pkl"
     df = pd.read_pickle(file_to_open)
 
     imagens = df.loc[: , "lamina"]
     labels = df.loc[: , "classificacao"]
+
+    encoder = LabelEncoder()
+    encoder.fit(labels)
+    Y_train = encoder.transform(labels)
+    Y_test = encoder.transform(labels)
+    Y_train = to_categorical(Y_train, num_classes = 13)
+
 
     # Conversao do dataset em array
     img2array = []
@@ -32,34 +39,21 @@ def load_dataset():
     return img_array, labels_array
 
 '''
-def label_encoder(df):
-    labels = ['Particulas corrosivas',
-              'Fibras',
-              'Esferas',
-              'Oxido preto',
-              'Oxido vermelho',
-              'Arrancamento',
-              'Mancal',
-              'Desgaste deslizamento',
-              'Atrito normal',
-              'Esferas contaminante',
-              'Areia / sujeira',
-              'Engrenagem',
-              'Degradacao do lubrificante']
+    labels = [("Particulas corrosivas"),
+              ("Fibras"),
+              ("Esferas"),
+              ("Oxido preto"),
+              ("Oxido vermelho"),
+              ("Arrancamento"),
+              ("Mancal"),
+              ("Desgaste deslizamento"),
+              ("Atrito normal"),
+              ("Esferas contaminante"),
+              ("Areia / sujeira"),
+              ("Engrenagem"),
+              ("Degradacao do lubrificante")]
+'''
 
-    encoder = LabelEncoder()
-    encoder.fit(labels)
-
-    for i, item in enumerate(encoder.classes_):
-        print(item, '=>', i)
-    
-    
-
-    #le.fit(df['classificacao'])
-    
-
-    return df
-    '''
 
 # Class weight
 def class_weight_dataset(y):
