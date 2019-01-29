60bdcf95f43d3ba75b55efa9b4dbbea2abad3066
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-01-29 15:37:24 -0200

    add plot

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
+#! /usr/bin/env python3
 import numpy as np
+import keras as KERAS
 from models import createModel
 from keras import backend as K
+import matplotlib.pyplot as plt
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+#from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from dataset import load_dataset, class_weight_dataset
+from dataset import load_dataset
 from keras.preprocessing.image import ImageDataGenerator
+from sklearn.utils.class_weight import compute_class_weight
 np.random.seed(42)
 
 # search Under and Over sampling or Class weight
 
 # (400 / 300) x 100 = new height      (100 x 113, 3)
-'''
-def sensitivity(Y_test, Y_pred):
-    true_positives = K.sum(K.round(K.clip(Y_test * Y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(Y_test, 0, 1)))
-    return true_positives / (possible_positives + K.epsilon())
-
-def specificity(Y_test, Y_pred):
-    true_negatives = K.sum(K.round(K.clip((1-Y_test) * (1-Y_pred), 0, 1)))
-    possible_negatives = K.sum(K.round(K.clip(1-Y_test, 0, 1)))
-    return true_negatives / (possible_negatives + K.epsilon())
-'''
 
 def train(img_array, labels_array):
     # Spliting train and test data
     X_train, X_test, Y_train, Y_test = train_test_split(img_array, labels_array)
 
+    
+    #class_weight_list = compute_class_weight('balanced', np.unique(Y_train), Y_train)
+    #class_weight = dict(zip(np.unique(Y_train), class_weight_list))
+    #X_train = X_train.astype('float64')
+    #X_test = X_test.astype('float64')
+
     # Normalization
     X_train = X_train / 255.0
     X_test = X_test / 255.0
 
-    class_weight = class_weight_dataset(Y_train)
-    
 
     # Data augmentation
     aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.15,
         height_shift_range=0.15, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
 
 
+    ##class_weight = class_weight_dataset(Y_train)
+    
     print("X_train shape: {}\nY_train shape: {}".format(X_train.shape, Y_train.shape))
     print("X_test shape: {}\nY_test shape: {}".format(X_test.shape, Y_test.shape))
 
@@ -49,10 +47,11 @@ def train(img_array, labels_array):
 
     trainmodel = createModel(IMG_DIMS)
     BATCH_SIZE = 3
-    EPOCHS = 100
+    EPOCHS = 3
     INIT_LR = 1e-3
     #opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-    trainmodel.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    opt = 'adam'
+    trainmodel.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
     '''
     # CHECKPOINT
@@ -66,28 +65,41 @@ def train(img_array, labels_array):
     history = trainmodel.fit_generator(
         aug.flow(X_train, Y_train, batch_size=BATCH_SIZE),
         epochs=EPOCHS, verbose=1, steps_per_epoch=len(X_train)//BATCH_SIZE,
-        validation_data=(X_test, Y_test), class_weight = class_weight)
+        validation_data=(X_test, Y_test))
 
     scores = trainmodel.evaluate(X_test, Y_test)
     print("\nAccuracy: %.2f%%" % (scores[1]*100))
 
-    y_pred = trainmodel.predict(X_test)
-    y_pred[y_pred > 0.5] = 1
-    y_pred[y_pred <= 0.5] = 0
+    y_pred = trainmodel.predict_classes(X_test)
+    print(y_pred)
 
-    print('\nConfusion Matrix')
-    matrix = confusion_matrix(Y_test.argmax(axis=1), y_pred.argmax(axis=1))
-    print(matrix)
+
+    # PLOT
+    # Accuracy curve
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    # Loss curve
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+
 
 '''
-y_test = np.argmax(Y_test, axis=1)
-y_pred = trainmodel.predict(X_test)
-
 # salva um arquivo do modelo
 trainmodel.save("model")'''
 
 dataset, labels = load_dataset()
-
 
 #if __name__ == '__main__':
 train(dataset, labels)
