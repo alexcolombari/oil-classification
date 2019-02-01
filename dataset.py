053749da0bb90c47ecd41943cc656e196dbdc83d
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-02-01 15:11:36 -0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from keras.utils.np_utils import to_categorical
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.utils.class_weight import compute_class_weight
 
 def load_dataset():
@@ -14,20 +15,15 @@ def load_dataset():
     imagens = df.loc[: , "lamina"]
     labels = df.loc[: , "classificacao"]
 
-    encoder = LabelEncoder()
-    encoder.fit(labels)
-    Y_train = encoder.transform(labels)
-    Y_test = encoder.transform(labels)
-    Y_train = to_categorical(Y_train, num_classes = 13)
-
-
     # Conversao do dataset em array
     img2array = []
     labels2array = []
 
-    for i in range(229):
+    #Autoencoder
+
+    for i in range(len(imagens)):
         imgarr = np.array(imagens[i])
-        img_resize = np.resize(imgarr, (10, 10, 3))  #100, 113, 3
+        img_resize = np.resize(imgarr, (300, 400, 3))  #100, 113, 3
         img2array.append(img_resize)
 
         labelsarr = np.asarray(labels[i])
@@ -52,13 +48,21 @@ def load_dataset():
               ("Areia / sujeira"),
               ("Engrenagem"),
               ("Degradacao do lubrificante")]
-'''
 
 
 # Class weight
-def class_weight_dataset(y):
-    y = y.flatten()
-    class_weight_list = compute_class_weight('balanced', np.unique(y), y)
-    class_weight = dict(zip(np.unique(y), class_weight_list))
+def class_weight_dataset(y_train, y_test):
+    encoder = LabelEncoder()
+    encoder.fit(y_train)
+    encoder.fit(y_test)
+    y_train = encoder.transform(y_train)
+    y_test = encoder.transform(y_test)
 
-    return class_weight
+    y_train = y_train.flatten()
+    class_weight_list = compute_class_weight('balanced', np.unique(y_train), y_train)
+    class_weight = dict(enumerate(np.unique(y_train), class_weight_list))
+    y_train = keras.utils.to_categorical(y_train, num_classes = len(np.unique(y_train)))
+    
+    return class_weight, y_train, y_test
+'''
+
