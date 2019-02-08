d6eea2ddefb5c1b52eb36a739ae01baa20e8fc78
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-02-08 15:29:48 -0200

    Update models.py

--- models.py
+++ models.py
@@ -5,15 +5,15 @@ def createModel(IMG_DIMS):
     # Encoder
     #input = 28 x 28 x 1 (wide and thin)
     model = Sequential()
-    model.add(Conv2D(4, (3, 3), activation='relu', padding='same', input_shape=IMG_DIMS))
+    model.add(Conv2D(2, (3, 3), activation='relu', padding='same', input_shape=IMG_DIMS))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(4, (3, 3), activation='relu', strides=(2, 2)))
+    model.add(Conv2D(2, (3, 3), activation='relu', strides=(2, 2)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(4, (3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(8, (3, 3), activation='relu', strides=(2, 2)))
+    model.add(Conv2D(4, (3, 3), activation='relu', strides=(2, 2)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
   
