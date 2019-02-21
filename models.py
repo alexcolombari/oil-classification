4a6e0832477282c622a5d85e2305a4fa43d705b0
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-02-21 14:56:26 -0300

    Update models.py

--- models.py
+++ models.py
@@ -7,16 +7,25 @@ def createModel(IMG_DIMS):
     model = Sequential()
     model.add(Conv2D(2, (3, 3), activation='relu', padding='same', input_shape=IMG_DIMS))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(2, (3, 3), activation='relu', strides=(2, 2)))
+    model.add(Conv2D(2, (3, 3), activation='relu')) #strides=(2, 2)
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    model.add(Conv2D(2, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(2, (3, 3), activation='relu'))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(4, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(4, (3, 3), activation='relu', strides=(2, 2)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(4, (3, 3), activation='relu'))
     model.add(Dropout(0.25))
-  
+    
+
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(8, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))    
+
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
