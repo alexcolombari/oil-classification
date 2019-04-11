d93129b94758579018f469463fd71aa137fbd8ad
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-04-11 13:22:39 -0300

    some adjusts

--- models.py
+++ models.py
@@ -3,57 +3,22 @@ from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampli
 
 def createModel(IMG_DIMS):
     # Encoder
-    #input = 28 x 28 x 1 (wide and thin)
     model = Sequential()
-    model.add(Conv2D(2, (3, 3), activation='relu', padding='same', input_shape=IMG_DIMS))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(2, (3, 3), activation='relu')) #strides=(2, 2)
+    model.add(Conv2D(12, (3, 3), activation='relu', padding='same', input_shape=IMG_DIMS))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     
-    model.add(Conv2D(2, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(2, (3, 3), activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Conv2D(24, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(24, (3, 3), activation='relu'))
+    model.add(Dropout(0.6))
 
-    model.add(Conv2D(4, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(4, (3, 3), activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Conv2D(48, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(48, (3, 3), activation='relu'))
+    model.add(Dropout(0.5))
     
-
-    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(8, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))    
-
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(13, activation='sigmoid'))
 
     return model
-   
-'''
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape = IMG_DIMS))
-    #model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
- 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    #model.add(Conv2D(64, (3, 3), activation='relu'))
-    #model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
- 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    #model.add(Conv2D(64, (3, 3), activation='relu'))
-    #model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
- 
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(13, activation='sigmoid'))
-
-    return model
-    '''
