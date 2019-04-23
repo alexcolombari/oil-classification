c4627a5a75e47a30c4c0b0265a51065e01c0896c
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-04-23 14:16:09 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,24 +1,21 @@
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D
+np.random.seed(42)
 
 def createModel(IMG_DIMS):
     # Encoder
     model = Sequential()
-    model.add(Conv2D(12, (3, 3), activation='relu', padding='same', input_shape=IMG_DIMS))
+    model.add(Conv2D(3, (3, 3), activation='relu', padding='same', input_shape = IMG_DIMS))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.2))
     
-    model.add(Conv2D(24, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(24, (3, 3), activation='relu'))
-    model.add(Dropout(0.6))
-
-    model.add(Conv2D(48, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(48, (3, 3), activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Conv2D(6, (5, 5), activation='relu', padding='same'))
+    model.add(Dropout(0.2))
     
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(Dense(13, activation='sigmoid'))
 
     return model
