5fa0f99308df9a43bd1ef2c8b026bada1bdd4a61
alexcolombari <38837753+alexcolombari@users.noreply.github.com>
2019-04-24 14:19:58 -0300

    Update models.py

--- models.py
+++ models.py
@@ -19,3 +19,23 @@ def createModel(IMG_DIMS):
     model.add(Dense(13, activation='sigmoid'))
 
     return model
+
+def createModel_2(IMG_DIMS):
+    model = Sequential()
+    model.add(Conv2D(3, (3, 3), activation = 'relu', padding = 'same', input_shape = IMG_DIMS))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(6, (5, 5), activation = 'relu', padding = 'same'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(3, (3, 3), activation = 'relu', padding = 'same'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation =' relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(13, activation = 'sigmoid'))
+
+    return model
