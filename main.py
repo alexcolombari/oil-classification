daa6f5f1b08378e1b2659b66b98a3651ba83f2b9
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-05-02 17:12:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 #! /usr/bin/env python3
-import numpy as np
+import os
 import math
-from models import createModel
-from keras import backend as K
+import numpy as np
+from models import *
+from metrics import *
 import matplotlib.pyplot as plt
 from dataset import load_dataset
-from keras.optimizers import Adam
+from keras.optimizers import Adam, Adadelta
+from keras.utils import plot_model
 from keras.models import load_model
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler, TensorBoard
 np.random.seed(42)
 
 def train(img_array, labels_array, class_weight):
@@ -26,20 +29,19 @@ def train(img_array, labels_array, class_weight):
 
     # DATA AUGMENTATION
     aug = ImageDataGenerator(rotation_range=50, width_shift_range=0.5, 
-            height_shift_range=0.66, shear_range=0.65, zoom_range=0.55,
+            height_shift_range=0.5, shear_range=0.60, zoom_range=0.55,
                 horizontal_flip=True, fill_mode="nearest")
-
-
 
     # INPUT SHAPE AND MODEL SETTINGS
     IMG_DIMS = (100, 200, 3)
     directory = "model-save/"
-    filepath = directory + "2_val_loss_model.h5"
-    model = createModel(IMG_DIMS)
-    #model = load_model(filepath)
-    BATCH_SIZE = 64
-    EPOCHS = 6000
+    filepath = directory + "5_val_loss_model.hdf5"
+    #model = createModel_4(IMG_DIMS)
+    model = load_model(filepath)
+    BATCH_SIZE = 128
+    EPOCHS = 2
 
+    '''
     # LEARNING RATE
     learning_rate = 0.001
     decay_rate = learning_rate / EPOCHS
@@ -51,20 +53,33 @@ def train(img_array, labels_array, class_weight):
         lrate = initial_lrate * math.pow(drop,
                 math.floor((1 + epoch) / epochs_drop))
         return lrate
+    '''
 
     # MODEL COMPILE
-    adam_opt = Adam(lr = learning_rate, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = decay_rate)
-    model.compile(optimizer = adam_opt, loss = 'binary_crossentropy', metrics=['accuracy'])
-    print(model.summary())
-
+    #adam_opt = Adam(lr = learning_rate, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = decay_rate)
+    #adadelta_opt = Adadelta(lr = 1.0, rho = 0.95, epsilon = None, decay = 0.0)
     
+    #model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+    print(model.summary())
+    #plot_model(model, to_file='model.png', show_shapes = True)
+
+    '''
+    # CLEAR TENSOBOARD DIRECTORY LOG FILES
+    mydir = "./logs/"
+    filelist = [ f for f in os.listdir(mydir) if f.endswith(".AIserver") ]
+    for f in filelist:
+        os.remove(os.path.join(mydir, f))
+    '''
+
     # CHECKPOINT
     checkpoint = ModelCheckpoint(filepath, monitor = 'val_loss', verbose = 1, save_best_only = True,
         mode='min')
-    early = EarlyStopping(monitor='val_loss', min_delta = 0, patience = 500, verbose = 0,
+    early = EarlyStopping(monitor='val_loss', min_delta = 0, patience = 950, verbose = 1,
         mode = 'min', restore_best_weights = True)
-    lrate = LearningRateScheduler(step_decay)    
-    callbacks_list = [checkpoint, early, lrate]
+    #lrate = LearningRateScheduler(step_decay)
+    tensor_board = TensorBoard(log_dir = './logs', histogram_freq = 2, batch_size = BATCH_SIZE,
+        write_graph = False, write_images = False, embeddings_layer_names = None, update_freq = 'epoch')    
+    callbacks_list = [checkpoint, tensor_board]
     
 
     # MODEL FIT WITH DATA AUG                   steps_per_epoch = len(X_train)//BATCH_SIZE
@@ -91,31 +106,26 @@ def train(img_array, labels_array, class_weight):
     Y_pred[Y_pred>=0.5] = 1
     Y_pred[Y_pred<0.5] = 0
 
-    '''
-    errors = []
-    for i in range(len(Y_pred)):
-        if Y_pred[i][1] != test_labels[i][1]:
-            errors.append([i, Y_pred_probabilities[i][0]])
-    sorted_errors = sorted(errors, key=lambda rec: rec[1], reverse=True)
-
-    print("\nNumber of errors = ", len(sorted_errors))
-    '''
     
     # PREDICT VS Y_TEST
-    print("\nY pred         3: {}".format(Y_pred[3]))
-    print("\nY test         3: {}".format(test_labels[3]))       
-    print("\nY pred        15: {}".format(Y_pred[15]))
-    print("\nY test        15: {}".format(test_labels[15])) 
-    print("\nY predicted   55: {}".format(Y_pred[55]))
-    print("\nY test        55: {}".format(test_labels[55]))
-    print("\nY predicted  231: {}".format(Y_pred[231]))
-    print("\nY test       231: {}".format(test_labels[231]))
-    print("\nY predicted  456: {}".format(Y_pred[456]))
-    print("\nY test       456: {}".format(test_labels[456]))
-    print("\nY predicted  755: {}".format(Y_pred[755]))
-    print("\nY test       755: {}".format(test_labels[755]))    
-    print("\nY predicted  932: {}".format(Y_pred[932]))
-    print("\nY test       932: {}".format(test_labels[932]))    
+    print("\nY pred          3: {}".format(Y_pred[3]))
+    print("\nY test          3: {}".format(test_labels[3]))       
+    print("\nY pred         26: {}".format(Y_pred[26]))
+    print("\nY test         26: {}".format(test_labels[26])) 
+    print("\nY predicted    55: {}".format(Y_pred[55]))
+    print("\nY test         55: {}".format(test_labels[55]))
+    print("\nY predicted   231: {}".format(Y_pred[231]))
+    print("\nY test        231: {}".format(test_labels[231]))
+    print("\nY predicted   456: {}".format(Y_pred[456]))
+    print("\nY test        456: {}".format(test_labels[456]))
+    print("\nY predicted   755: {}".format(Y_pred[755]))
+    print("\nY test        755: {}".format(test_labels[755]))    
+    print("\nY predicted   932: {}".format(Y_pred[932]))
+    print("\nY test        932: {}".format(test_labels[932]))
+    print("\nY predicted  1055: {}".format(Y_pred[1055]))
+    print("\nY test       1055: {}".format(test_labels[1055]))
+    print("\nY predicted  1188: {}".format(Y_pred[1188]))
+    print("\nY test       1188: {}".format(test_labels[1188]))                     
     
        
     # PLOT
@@ -141,7 +151,6 @@ def train(img_array, labels_array, class_weight):
     #plt.show()
     
 
-
-dataset, labels, class_weight = load_dataset()
-
-train(dataset, labels, class_weight)
+if __name__ == "__main__":
+    dataset, labels, class_weight = load_dataset()
+    train(dataset, labels, class_weight)
