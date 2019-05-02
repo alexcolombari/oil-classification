ff0aa3c8cfa54397f56392e4ba9013fead8c324f
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-05-02 17:13:15 -0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,12 +1,28 @@
 import pandas as pd
+import numpy as np
+from PIL import Image
+import random
 
 # Generate Pickle archive
 
 data_folder = "/opt/data_repository/oil_samples/"
 file_to_open = data_folder + "laminas.pkl"
+#file_to_open = "laminas-dez.pkl"
 df = pd.read_pickle(file_to_open)
 
-aa = df.iloc[:4000, :]
+for i in df:
+    numero = random.randint(0, 8000)
 
-#aa.pivot(index='id', columns='classificacao', values='lamina')
-aa.to_pickle("/opt/data_repository/oil_samples/half-samples.pkl")
+def createImage():
+    aa = df.loc[numero, "lamina"]
+    aa.save('test_image.jpg')
+    print("[INFO] DONE!")
+
+def getVetor():
+    ab = df.loc[numero, "classificacao"]
+    vetor = np.zeros((1, 13))
+    vetor += np.array(ab)
+    return vetor
+
+#aa.to_pickle(data_folder + "7500-samples.pkl")
+createImage()
