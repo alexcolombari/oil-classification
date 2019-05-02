75bcae058ed9c29e82ded4e2b9166e421a4f1975
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-05-02 17:13:32 -0300

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -4,22 +4,26 @@
 import numpy as np
 import pandas as pd
 from PIL import Image
+from generate import *
+from keras import backend as K
 from keras.models import load_model
 
 def test_model():
+    dataframe = createImage()
     # LOAD IMAGE
     imagem = Image.open('test_image.jpg')
     imagem = np.array(imagem)
-    imagem = np.resize(imagem, (1, 100,200,3))
+    imagem = np.resize(imagem, (1, 100, 200, 3))
+
+    imagem = imagem / 255.0
     
     array_imagem = []
     array_imagem.append(imagem)
     array_imagem = np.expand_dims(array_imagem, axis = 1)
-    print(array_imagem)
 
     # LOAD MODEL
     model_filepath = "model-save/"
-    model_name = model_filepath + "2_val_loss_model.h5"
+    model_name = model_filepath + "5_val_loss_model.hdf5"
     model = load_model(model_name)
 
     # MODEL PREDICT
@@ -27,6 +31,12 @@ def test_model():
     pred[pred>=0.5] = 1
     pred[pred<0.5] = 0
 
-    print("Predict Label: {}".format(pred))
+    vetor = getVetor()
+    vetor[vetor>=0.5] = 1
+    vetor[vetor<0.5] = 0
+
+    print("Predict Label:  {}".format(pred))
+    print("Original Label: {}".format(vetor))
+
 
 test_model()
