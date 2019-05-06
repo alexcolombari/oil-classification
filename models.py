85383c9a6e58a83be2d8d4eadb9398c61bd28ab3
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-05-06 16:11:27 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,75 +1,22 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D, BatchNormalization
-np.random.seed(42)
+np.random.seed(7)
 
 def createModel(IMG_DIMS):
-    # Encoder
-    model = Sequential()
-    model.add(Conv2D(3, (3, 3), activation = 'relu', padding = 'same', input_shape = IMG_DIMS))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Dropout(0.2))
-    
-    model.add(Conv2D(6, (5, 5), activation = 'relu', padding = 'same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
-    
-    model.add(Flatten())
-    model.add(Dense(512, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(13, activation = 'sigmoid'))
-
-    return model
-
-def createModel_2(IMG_DIMS):
-    model = Sequential()
-    model.add(Conv2D(3, (3, 3), activation = 'relu', padding = 'same', input_shape = IMG_DIMS))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
-    model.add(Conv2D(6, (5, 5), activation = 'relu', padding = 'same'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Dropout(0.3))
-
-    model.add(Conv2D(3, (3, 3), activation = 'relu', padding = 'same'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-
-    model.add(Flatten())
-    model.add(Dense(512, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(13, activation = 'sigmoid'))
-
-    return model
-
-def createModel_3(IMG_DIMS):
-    model = Sequential()
-    model.add(Conv2D(6, (3, 3), activation = 'relu', padding = 'same', input_shape = IMG_DIMS))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
-    model.add(Conv2D(3, (5, 5), activation = 'relu', padding = 'same'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Dropout(0.3))
-
-    model.add(Conv2D(3, (3, 3), activation = 'relu', padding = 'same'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-
-    model.add(Flatten())
-    model.add(Dense(512, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(13, activation = 'sigmoid'))
-
-    return model
-
-def createModel_4(IMG_DIMS):
     model = Sequential()
     model.add(Conv2D(6, (3, 3), activation = 'relu', padding = 'same', input_shape = IMG_DIMS))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Dropout(0.2))
-    model.add(Conv2D(12, (3, 3), activation = 'relu', padding = 'same'))
-   
+    
+    model.add(Conv2D(6, (3, 3), activation = 'relu', padding = 'same'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+
     model.add(Conv2D(12, (3, 3), activation = 'relu', padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(12, (3, 3), activation = 'relu', padding = 'same'))   
     model.add(Dropout(0.3))
 
     model.add(Conv2D(12, (3, 3), activation = 'relu', padding = 'same'))   
@@ -78,8 +25,28 @@ def createModel_4(IMG_DIMS):
     model.add(Flatten())
     model.add(Dense(512, activation = 'relu'))
     model.add(Dropout(0.3))
-    model.add(Dense(256, activation = 'relu'))
-    model.add(Dropout(0.3))
     model.add(Dense(13, activation = 'sigmoid'))
 
     return model
+
+def encoder_model(input_img):
+    # ENCODER
+    #input = 100 x 200 x 1 (wide and thin)
+    conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img) # 100 x 200 x 16
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) # 10 x 200 x 16
+    
+    conv2 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool1) # 50 x 100 x 8
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) # 50 x 100 x 8
+    
+    conv3 = Conv2D(4, (3, 3), activation='relu', padding='same')(pool2) # 25 x 50 x 4 (small and thick)
+
+    # DECODER
+    conv4 = Conv2D(4, (3, 3), activation='relu', padding='same')(conv3) # 25 x 50 x 4
+    up1 = UpSampling2D((2,2))(conv4) # 25 x 50 x 128
+    
+    conv5 = Conv2D(8, (3, 3), activation='relu', padding='same')(up1) # 50 x 100 x 8
+    up2 = UpSampling2D((2,2))(conv5) # 50 x 100 x 64
+    
+    decoded = Conv2D(1, (3, 3), activation='relu', padding='same')(up2) # 100 x 200 x 16
+    
+    return decoded
