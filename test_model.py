fd5562e4bc6373d2cadf56ee5b65e4200cf81438
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-05-08 15:07:30 -0300

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -5,29 +5,45 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from generate import *
+from keras.layers import Input
 from keras import backend as K
-from keras.models import load_model
+from keras.models import load_model, Model
 
 def test_model():
     dataframe = createImage()
+    
     # LOAD IMAGE
     imagem = Image.open('test_image.jpg')
     imagem = np.array(imagem)
-    imagem = np.resize(imagem, (1, 100, 200, 3))
+    imagem = np.resize(imagem, (1, 100, 200, 1))
 
     imagem = imagem / 255.0
+
     
-    array_imagem = []
-    array_imagem.append(imagem)
-    array_imagem = np.expand_dims(array_imagem, axis = 1)
+    # MODEL LOAD
+    model_directory = "model-save/"
+    model_path = model_directory + "auto_encoder_model.hdf5"
+    autoencoder_model = load_model(model_path)
+
+    # INPUT SHAPE AND MODEL SETTINGS
+    x, y = 25, 50
+    inChannel = 3
+    input_img = Input(shape = (x, y, inChannel))
+    
+    # GET AUTOENCODED MODEL LAYERS
+    encoder = Model(autoencoder_model.input, autoencoder_model.layers[-6].output)
+
+    x_train_predict = encoder.predict(imagem)
+    x_train_predict = np.resize(x_train_predict, (len(x_train_predict), 25, 50, 3)) 
+
 
     # LOAD MODEL
     model_filepath = "model-save/"
-    model_name = model_filepath + "5_val_loss_model.hdf5"
+    model_name = model_filepath + "trained_model.hdf5"
     model = load_model(model_name)
 
     # MODEL PREDICT
-    pred = model.predict(imagem)
+    pred = model.predict(x_train_predict)
     pred[pred>=0.5] = 1
     pred[pred<0.5] = 0
 
@@ -38,5 +54,7 @@ def test_model():
     print("Predict Label:  {}".format(pred))
     print("Original Label: {}".format(vetor))
 
+    return pred, vetor
 
-test_model()
+if __name__ == "__main__":
+    test_model()
