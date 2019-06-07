d5a47a05a945ac7361cf55be99a39d84c62afa3a
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-06-07 16:51:27 -0300

    Update train_encoder.py

--- train_encoder.py
+++ train_encoder.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 from PIL import Image
+from io import BytesIO  
 from keras import optimizers
+from keras import backend as K
 import matplotlib.pyplot as plt
 from dataset import load_dataset
 from keras.utils import plot_model
@@ -9,64 +11,62 @@ from keras.models import load_model, Model
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D, BatchNormalization
+from keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D
 np.random.seed(7)
 
 def autoencoder(input_img):
-    #encoder
-    #input = 28 x 28 x 1 (wide and thin)
-    conv1 = Conv2D(4, (3, 3), activation='relu', padding='same')(input_img) #176 x 176 x 32
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #88 x 88 x 32
-    conv2 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool1) #88 x 88 x 64
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #44 x 44 x 64
-    conv3 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool2) #44 x 44 x 128 (small and thick)
+    x1 = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x2 = MaxPooling2D((2, 2), padding='same')(x1)
+    x3 = Dropout(0.3)(x2)
+    x4 = Conv2D(8, (3, 3), activation='relu', padding='same')(x3)
+    encoded = MaxPooling2D((2, 2), padding='same')(x4)
 
-    #decoder
-    conv4 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv3) #44 x 44 x 128
-    up1 = UpSampling2D((2,2))(conv4) # 88 x 88 x 128
-    conv5 = Conv2D(8, (3, 3), activation='relu', padding='same')(up1) # 88 x 88 x 64
-    up2 = UpSampling2D((2,2))(conv5) # 176 x 176 x 64
-    decoded = Conv2D(3, (3, 3), activation='relu', padding='same')(up2) # 176 x 176 x 1
+    print("shape of encoded", K.int_shape(encoded))
 
+    x5 = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x6 = UpSampling2D((2, 2))(x5)
+    x7 = Conv2D(8, (3, 3), activation='relu', padding='same')(x6)
+    x8 = UpSampling2D((2, 2))(x7)
+    decoded = Conv2D(1, (5, 5), activation='sigmoid', padding='same')(x8)
+    print("shape of decoded", K.int_shape(decoded))
     return decoded
 
-
+ 
 # LOAD DATASET
 data_folder = "/opt/data_repository/oil_samples/"
 file_to_open = data_folder + "laminas.pkl"
 
 df = pd.read_pickle(file_to_open)
+imagens = df.loc[:3999 , "lamina"]
 
-imagens = df.loc[0:99 , "lamina"]
-#imagens = df.loc[0:9199 , "lamina"]
 
 # CONVERSION TO ARRAY
 for i in range(len(imagens)):
-        imgarr = np.array(imagens[i])
-img_array = np.asarray(imgarr)
+    imgarr = np.array(imagens[i])
+    
+imgarr = np.resize(imgarr, (len(imagens), 300, 400, 1))
+
 
 # SPLIT
-train_data, test_data = train_test_split(img_array)
-
-train_data = np.resize(train_data, (len(train_data), 300, 400, 3))
-test_data = np.resize(test_data, (len(test_data), 300, 400, 3))
+train_data, test_data = train_test_split(imgarr)
+#train_data = np.resize(train_data, (len(train_data), 300, 400, 1))
+#test_data = np.resize(test_data, (len(test_data), 300, 400))
 
 train_data = train_data.astype('uint8') / 255.0
 test_data = test_data.astype('uint8') / 255.0
 
 
-input_img = Input(shape=(300, 400, 3)) # 176, 176, 1
-
+input_img = Input(shape=(300, 400, 1))  # input_img = Input(shape=(300, 400, 3))
 autoencoder = Model(input_img, autoencoder(input_img))
 
-autoencoder.compile(optimizer = 'adam', loss = 'mean_absolute_error', metrics = ['accuracy'])
+autoencoder.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 #plot_model(autoencoder, to_file='model_encoder.png', show_shapes = True)
 print(autoencoder.summary())
 
 directory = "model-save/"
-filepath = directory + "auto_encoder_model.hdf5"
+filepath = directory + "2_auto_encoder_model.hdf5"
 
-epochs = 1000
+epochs = 4500
 batch_size = 64
 patience = (epochs * 10) / 100
 
@@ -97,3 +97,6 @@ plt.savefig('encoder_loss_curve.png')
 # val loss (adam / mse) = 0.00118
 # ----------------------------------
 # val loss (adam / mae) = 0.03445 // accuracy = 0.9994 (1000 epochs)
+
+# 30/05 val loss (adadelta / mse) = 0.00968 // accuracy = 0.9712 (15000 epochs)
+# 07/06 val loss (adam / mse) = 0.00265 (4500 epochs)
