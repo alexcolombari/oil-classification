4b3ebb5cfffc315f5713a598c193622031324101
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-07-02 13:56:37 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,19 +1,17 @@
 import numpy as np
-from keras import regularizers
+from keras.constraints import max_norm
+from keras import regularizers, initializers
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D, BatchNormalization
 np.random.seed(7)
 
 def model_1(input_img):
-    input_layer = Conv2D(2, (3, 3), activation = 'relu', padding = 'same')(input_img)
+    input_layer = Conv2D(2, (3, 3), activation = 'relu', padding = 'same',
+                        bias_initializer=initializers.RandomNormal(0.1),
+                        kernel_constraint=max_norm(2.))(input_img)
     x = Dropout(0.3)(input_layer)
-    x = Conv2D(4, (3, 3), activation = 'relu', padding = 'same')(x)
-    x = Dropout(0.3)(x)
-    x = Conv2D(4, (3, 3), activation = 'relu', padding = 'same')(x)
-    x = Dropout(0.3)(x)
-    
+
     x = Flatten()(x)
-    x = Dense(512, activation = 'relu')(x)
-    x = Dropout(0.45)(x)
-    output = Dense(13, activation = 'sigmoid')(x)
+    output = Dense(13, activation = 'sigmoid', bias_initializer=initializers.RandomNormal(0.1),
+                    kernel_constraint=max_norm(2.))(x)
 
     return output
