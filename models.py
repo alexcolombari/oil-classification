6ab089fad6c61d31d77ba1daee56738b4402cc67
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-08-27 17:05:59 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,21 @@
 import numpy as np
+
+from keras.models import Sequential
 from keras.constraints import max_norm
 from keras import regularizers, initializers
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D, BatchNormalization
+from keras.layers import Dense, Conv2D, MaxPool2D, Dropout, Flatten
+
 np.random.seed(7)
 
 def cnn(input_img):
-    input_layer = Conv2D(2, (3, 3), activation = 'relu', padding = 'same',
-                        bias_initializer=initializers.RandomNormal(0.1),
-                        kernel_constraint=max_norm(2.))(input_img)
-    x = Dropout(0.3)(input_layer)
+    model = Sequential()
+    model.add(Conv2D(16, (2, 2), input_shape = input_img, activation = 'relu', padding = 'same', kernel_regularizer = regularizers.l2(1e-7)))
+    model.add(MaxPool2D(pool_size = (2, 2)))
+    model.add(Dropout(0.3))
+    model.add(Conv2D(13, (2, 2), activation = 'relu', padding = 'same', kernel_regularizer = regularizers.l2(1e-7)))
+    model.add(MaxPool2D(pool_size = (2, 2)))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(13, activation = 'sigmoid', kernel_regularizer = regularizers.l2(1e-7)))
 
-    x = Flatten()(x)
-    output = Dense(13, activation = 'sigmoid', bias_initializer=initializers.RandomNormal(0.1),
-                    kernel_constraint=max_norm(2.))(x)
-
-    return output
+    return model
